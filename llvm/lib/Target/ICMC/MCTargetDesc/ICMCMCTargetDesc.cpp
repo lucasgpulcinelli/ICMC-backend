d9df1b4cf11c24a4093afbd78219214cea7af639
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-11-30 22:15:42 -0300

    Adding a more correct register info implementation

--- llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.cpp
+++ llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.cpp
@@ -1,22 +1,35 @@
+#include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 
 #include "ICMCMCTargetDesc.h"
 #include "ICMCTargetInfo.h"
 
+#define GET_REGINFO_ENUM
+#define GET_REGINFO_MC_DESC
+#include "ICMCGenRegisterInfo.inc"
+
+
 using namespace llvm;
 
 ICMCMCAsmInfo::ICMCMCAsmInfo(const Triple &TT, const MCTargetOptions &Options){
-  CodePointerSize = 2;
-  CalleeSaveStackSlotSize = 2;
-  CommentString = ";";
-  PrivateGlobalPrefix = ".L";
-  PrivateLabelPrefix = ".L";
-  UsesELFSectionDirectiveForBSS = true;
-  SupportsDebugInformation = true;
+    CodePointerSize = 2;
+    CalleeSaveStackSlotSize = 2;
+    CommentString = ";";
+    PrivateGlobalPrefix = ".L";
+    PrivateLabelPrefix = ".L";
+    UsesELFSectionDirectiveForBSS = true;
+    SupportsDebugInformation = true;
+}
+
+static MCRegisterInfo *createICMCMCRegisterInfo(const Triple &TT) {
+    MCRegisterInfo *X = new MCRegisterInfo();
+    InitICMCMCRegisterInfo(X, 0);
+    return X;
 }
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeICMCTargetMC() {
     Target T = getTheICMCTarget();
     RegisterMCAsmInfo<ICMCMCAsmInfo> X(T);
+    TargetRegistry::RegisterMCRegInfo(T, createICMCMCRegisterInfo);
 }
 
