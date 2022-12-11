79219c62a7029585b5dd9b4f3668bbadc355f1ae
beatrizdiniz <dinyzbya@gmail.com>
2022-12-11 15:37:39 -0300

    add, rts instructions

--- llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.cpp
+++ llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.cpp
@@ -1,3 +1,5 @@
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 
@@ -7,6 +9,10 @@
 #define GET_REGINFO_ENUM
 #define GET_REGINFO_MC_DESC
 #include "ICMCGenRegisterInfo.inc"
+
+#define GET_INSTRINFO_MC_DESC
+#define ENABLE_INSTR_PREDICATE_VERIFIER
+#include "ICMCGenInstrInfo.inc"
 
 
 using namespace llvm;
@@ -24,13 +30,19 @@ ICMCMCAsmInfo::ICMCMCAsmInfo(const Triple &TT, const MCTargetOptions &Options){
 static MCRegisterInfo *createICMCMCRegisterInfo(const Triple &TT) {
     MCRegisterInfo *X = new MCRegisterInfo();
     InitICMCMCRegisterInfo(X, ICMC::SP, 0, 0, ICMC::PC);
-    printf("%p\n", (void*) X);
+    return X;
+}
+
+static MCInstrInfo *createICMCMCInstrInfo(void) {
+    MCInstrInfo *X = new MCInstrInfo();
+    InitICMCMCInstrInfo(X);
     return X;
 }
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeICMCTargetMC() {
     Target T = getTheICMCTarget();
     RegisterMCAsmInfo<ICMCMCAsmInfo> X(T);
+    TargetRegistry::RegisterMCInstrInfo(T, createICMCMCInstrInfo);
     TargetRegistry::RegisterMCRegInfo(T, createICMCMCRegisterInfo);
 }
 
