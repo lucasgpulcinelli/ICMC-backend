cc24629d61b4f1d2b3a1660e2b0bd4699db1deb4
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2023-03-24 14:05:32 -0300

    starting MCTargetDesc

--- llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.cpp
+++ llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.cpp
@@ -1,48 +1,91 @@
-#include "llvm/MC/MCInstrInfo.h"
-#include "llvm/MC/MCSubtargetInfo.h"
-#include "llvm/MC/MCRegisterInfo.h"
-#include "llvm/MC/TargetRegistry.h"
-
 #include "ICMCMCTargetDesc.h"
-#include "ICMCTargetInfo.h"
+#include "ICMCMCAsmInfo.h"
+#include "ICMCTargetStreamer.h"
+#include "ICMCInstrPrinter.h"
+#include "TargetInfo/ICMCTargetInfo.h"
+#include "ICMCAsmBackend.h"
 
-#define GET_REGINFO_ENUM
-#define GET_REGINFO_MC_DESC
-#include "ICMCGenRegisterInfo.inc"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+
+using namespace llvm;
 
 #define GET_INSTRINFO_MC_DESC
 #define ENABLE_INSTR_PREDICATE_VERIFIER
 #include "ICMCGenInstrInfo.inc"
 
+#define GET_SUBTARGETINFO_MC_DESC
+#include "ICMCGenSubtargetInfo.inc"
 
-using namespace llvm;
+#define GET_REGINFO_MC_DESC
+#include "ICMCGenRegisterInfo.inc"
 
-ICMCMCAsmInfo::ICMCMCAsmInfo(const Triple &TT, const MCTargetOptions &Options){
-    CodePointerSize = 2;
-    CalleeSaveStackSlotSize = 2;
-    CommentString = ";";
-    PrivateGlobalPrefix = ".L";
-    PrivateLabelPrefix = ".L";
-    UsesELFSectionDirectiveForBSS = true;
-    SupportsDebugInformation = true;
+
+static MCAsmInfo *createICMCMCAsmInfo(const MCRegisterInfo &MRI,
+                                      const Triple &TT,
+                                      const MCTargetOptions &Options) {
+  MCAsmInfo *MAI = new ICMCMCAsmInfo(TT);
+
+  return MAI;
+}
+
+static MCInstrInfo *createICMCMCInstrInfo() {
+  auto *X = new MCInstrInfo();
+  InitICMCMCInstrInfo(X);
+  return X;
 }
 
 static MCRegisterInfo *createICMCMCRegisterInfo(const Triple &TT) {
-    MCRegisterInfo *X = new MCRegisterInfo();
-    InitICMCMCRegisterInfo(X, ICMC::SP, 0, 0, ICMC::PC);
-    return X;
+  auto *X = new MCRegisterInfo();
+  InitICMCMCRegisterInfo(X, ICMC::R0);
+  return X;
 }
 
-static MCInstrInfo *createICMCMCInstrInfo(void) {
-    MCInstrInfo *X = new MCInstrInfo();
-    InitICMCMCInstrInfo(X);
-    return X;
+static MCSubtargetInfo *createICMCMCSubtargetInfo(const Triple &TT,
+                                                  StringRef CPU, StringRef FS) {
+  return createICMCMCSubtargetInfoImpl(TT, CPU, CPU, FS);
+}
+
+static MCInstPrinter *createICMCMCInstPrinter(const Triple &T,
+                                             unsigned SyntaxVariant,
+                                             const MCAsmInfo &MAI,
+                                             const MCInstrInfo &MII,
+                                             const MCRegisterInfo &MRI) {
+  return new ICMCInstPrinter(MAI, MII, MRI);
+}
+
+static MCTargetStreamer *createTargetAsmStreamer(MCStreamer &S,
+                                                 formatted_raw_ostream &OS,
+                                                 MCInstPrinter *InstPrint,
+                                                 bool isVerboseAsm) {
+  return new ICMCTargetStreamer(S);
+}
+
+MCAsmBackend *createICMCAsmBackend(const Target &T, const MCSubtargetInfo &STI,
+                                  const MCRegisterInfo &MRI,
+                                  const llvm::MCTargetOptions &TO) {
+  return new ICMCAsmBackend(STI.getTargetTriple().getOS());
 }
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeICMCTargetMC() {
-    Target T = getTheICMCTarget();
-    RegisterMCAsmInfo<ICMCMCAsmInfo> X(T);
-    TargetRegistry::RegisterMCInstrInfo(T, createICMCMCInstrInfo);
-    TargetRegistry::RegisterMCRegInfo(T, createICMCMCRegisterInfo);
+  Target &TheICMCTarget = getTheICMCTarget();
+  RegisterMCAsmInfoFn X(TheICMCTarget, createICMCMCAsmInfo);
+
+  TargetRegistry::RegisterMCInstrInfo(TheICMCTarget, createICMCMCInstrInfo);
+
+  TargetRegistry::RegisterMCRegInfo(TheICMCTarget, createICMCMCRegisterInfo);
+
+  TargetRegistry::RegisterMCSubtargetInfo(TheICMCTarget,
+                                          createICMCMCSubtargetInfo);
+
+  TargetRegistry::RegisterMCInstPrinter(TheICMCTarget,
+                                        createICMCMCInstPrinter);
+
+  TargetRegistry::RegisterAsmTargetStreamer(TheICMCTarget,
+                                            createTargetAsmStreamer);
+
+  TargetRegistry::RegisterMCAsmBackend(TheICMCTarget, createICMCAsmBackend);
 }
 
--- llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.h
+++ llvm/lib/Target/ICMC/MCTargetDesc/ICMCMCTargetDesc.h
@@ -1,18 +1,22 @@
 #ifndef LLVM_ICMC_MC_TARGET_DESC_H
 #define LLVM_ICMC_MC_TARGET_DESC_H
 
-#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/Support/DataTypes.h"
 
 namespace llvm {
 
-class Triple;
+class Target;
 
-class ICMCMCAsmInfo : public MCAsmInfo {
-public:
-    explicit ICMCMCAsmInfo(const Triple &TT, const MCTargetOptions &Options);
-};
+} // end namespace llvm
 
-}
+#define GET_REGINFO_ENUM
+#include "ICMCGenRegisterInfo.inc"
+
+#define GET_INSTRINFO_ENUM
+#define GET_INSTRINFO_MC_HELPER_DECLS
+#include "ICMCGenInstrInfo.inc"
+
+#define GET_SUBTARGETINFO_ENUM
+#include "ICMCGenSubtargetInfo.inc"
 
 #endif
-
